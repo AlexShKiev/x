a0a45a3098f9bb457e0b40d688c51a4455f14eee
admanashy <a.shypko88@gmail.com>
2016-09-22 21:46:00 +0300

    added object creation

--- src/python/login.py
+++ src/python/login.py
@@ -1,9 +1,8 @@
-import unittest
+#import unittest
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 
-class LoginEnv (unittest.TestCase):
+
+class LoginEnv:
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
@@ -12,16 +11,19 @@ class LoginEnv (unittest.TestCase):
         cls.driver.get("http://sta-kiv-gt2-setup01-spp-01.nix.cydmodule.com:8080/admin/tester.jsp")
 
     def log_to_env(self):
-
-        username=self.driver.find_element_by_css_selector("input[name=login]")
-        passw=self.driver.find_element_by_name("password")
+        username = self.driver.find_element_by_css_selector("input[name=login]")
+        passw = self.driver.find_element_by_name("password")
 
         username.send_keys("manager")
         passw.send_keys("manager")
 
         self.driver.find_element_by_css_selector("input[type=submit]").click()
 
-
     @classmethod
     def tearDown(self):
         self.driver.quit()
+
+if __name__ == "__main__":
+    le = LoginEnv()
+    le.setUpClass()
+    le.log_to_env()
